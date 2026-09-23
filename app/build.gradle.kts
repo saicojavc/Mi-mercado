baada70665cf88b3f083328cec7f0b96ec3a38dc
Jorge Adrian Valdes Camacho <jorgito.avc03@gmail.com>
2026-09-22 23:02:11 -0400

    Bump version to 1.0.1 and update IDE deployment target selection
    - Increment `versionCode` to 2 and `versionName` to "1.0.1" in the app module build configuration.
    - Add a new run configuration selection state for "main" in the IDE deployment target selector, targeting an emulator and two physical devices.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -24,8 +24,8 @@ android {
         applicationId = "com.saico.mimercado"
         minSdk = libs.versions.minSdk.get().toInt()
         targetSdk = libs.versions.targetSdk.get().toInt()
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         buildConfigField("String", "DATABASE_URL", "\"$dbUrl\"")
